8af5cbd3e5195fa2a57cf9b069e8d8b8b5bc2119
Joey <me@joeygaskin.com>
2015-11-28 20:23:52 -0500

    Update 2015-11-29T01:23:49.522Z

--- app/scripts/models/images-collection.js
+++
@@ -1,8 +0,0 @@
-import Image from 'models/image';
-
-var ImagesCollection = Backbone.Collection.extend({
-  model: Image,
-  url: "http://tiny-lasagna-server.herokuapp.com/collections/images"
-});
-
-export default ImagesCollection;
